f4aad64218d01897a6e4d0ad15492655be1459f6
gunesserkan <gunesserkan883@gmail.com>
2025-03-12 00:07:21 +0300

    Second day examples and practice code

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
-    "use strict";
+	"sap/ui/core/mvc/Controller"
+], (Controller) => {
+	"use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.App", {
-        onShowHello() {
-            MessageToast.show("Hello World");
-        }
-    });
+	return Controller.extend("ui5.walkthrough.controller.App", {});
 });
